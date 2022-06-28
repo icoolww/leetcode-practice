d4b6c4717a63c14a499b0188be05bcc5f7918a64
William <williamwijaya.mail@gmail.com>
2022-06-28 16:38:09 -0700

    Adding roman to integer solution

--- roman_to_integer.js
+++ roman_to_integer.js
@@ -12,10 +12,12 @@ var romanToInt = function(s) {
   
   let output = 0;
   for (let i = 0; i < s.length; i++) {
+      // handling substraction
       if (table[s[i]] < table[s[i+1]]) {
           output -= table[s[i]];
       }
    else {
+    // handling addition 
       output += table[s[i]];
    }
       
