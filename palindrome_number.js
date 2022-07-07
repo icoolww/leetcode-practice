320619da00feeb722cb3be146a061a79991f1843
William <williamwijaya.mail@gmail.com>
2022-07-07 14:28:10 -0700

    Adding palindrome number solution

--- palindrome_number.js
+++ palindrome_number.js
@@ -2,15 +2,17 @@
 // iterate, read the number from right to left and compare it to (x)
 
 var isPalindrome = function(x) {
-    // handling negative
-    if (x < 0) {
-      return false;
-
-    // handling from right to left
-    let rightToLeft = Number(x.toString().split("").reverse().join(""));
-    if (x === rightToLeft){
-      return true;
-    }
-    }
+  // handling negative number
+  if (x < 0) {
     return false;
+  }
+  // handling from right to left
+  
+  // let rightToLeft = Number(x.toString().reverse().split("").join(""));
+  
+  let rightToLeft = Number(x.toString().split("").reverse().join(""));
+  if (x === rightToLeft){
+    return true;
+  }
+  return false;
 };
