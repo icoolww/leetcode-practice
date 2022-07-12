cde7b979c4bc9a5df2ab601efe64a503635c1ce8
William <williamwijaya.mail@gmail.com>
2022-07-12 16:47:36 -0700

    Adding valid parentheses solution

--- valid_parentheses.js
+++ valid_parentheses.js
@@ -1,15 +1,31 @@
-// 
+var isValid = function (s) {
+  // return false if empty
+  if (s.length === 0) return false;
 
-var isValid = function(s) {
+  //creating empty stack to push the bracket later
+  let stack = [];
 
-  // defining the list of parentheses
-  let brackets = {
-    "(": ")",
-    "{" : "}",
-    "[": "]"
+  // iterate through loop, push the left side to the stack,
+  for (let bracket of s) {
+    if (bracket === "(" || bracket === "{" || bracket === "[") {
+      stack.push(bracket);
     }
+    // if left bracket isnt equal with the stack, return false;
+    else {
+      let leftBracket = stack.pop();
+      if (bracket === ")" && leftBracket !== "(") {
+        return false;
+      }
+      if (bracket === "}" && leftBracket !== "{") {
+        return false;
+      }
+      if (bracket === "]" && leftBracket !== "[") {
+        return false;
+      }
+    }
+  }
 
-    
+  if (stack.length > 0) return false;
+
+  return true;
 };
-
-
