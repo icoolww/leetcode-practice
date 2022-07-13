46d72018e442f9d533b1f9b90491a97d128e6161
William <williamwijaya.mail@gmail.com>
2022-07-12 18:31:40 -0700

    Adding insert position solution

---
+++ insert_position.js
@@ -0,0 +1,11 @@
+// iterate nums
+// if nums is biiger than target, return the position
+
+var searchInsert = function(nums, target) {
+  for (let i = 0; i < nums.length; i++){
+      if (nums[i] >= target){
+          return i;
+      }
+  }
+  return nums.length;
+};
