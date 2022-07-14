6d8c1dbc318433007a74a6596fcbfbaa2764968b
William <williamwijaya.mail@gmail.com>
2022-07-14 14:51:18 -0700

    Adding plus one solution

--- plus_one.js
+++ plus_one.js
@@ -1,3 +1,26 @@
+// iterate the length through the loops
+
 var plusOne = function(digits) {
-  let output = [];
+
+  // handling example 1
+  for (let i = digits.length - 1; i >= 0; i-- ) {
+      if (i = digits.length - 1) {
+        digits[i] += 1;
+      }
+      
+      // handling example 3
+      if (digits > 9 && digits > 0) {
+        digits[i-1] += 1;
+        digits[i] = 0;
+      }
+
+      // handling number 10 to eliminate number 1
+      if (digits[0] === 10 ) {
+        digits[0] = 0;
+        digits.unshift(1);
+
+      }
+
+  }
+  return digits;
 };
